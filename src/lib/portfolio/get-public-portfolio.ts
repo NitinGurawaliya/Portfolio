9a8681e1cf5fadfa9268be010e806ef0e980225f
NitinGurawaliya <221030201@juitsolan.in>
2025-12-05 22:36:57 +0530

    build fix final 2

--- src/lib/portfolio/get-public-portfolio.ts
+++ src/lib/portfolio/get-public-portfolio.ts
@@ -27,7 +27,7 @@ export async function getPublicPortfolio(username: string) {
     const cacheCheckTime = performance.now() - startTime
     
     const dbQueryStart = performance.now()
-    let portfolio = await prisma.portfolio.findFirst({
+    let portfolio: any = await prisma.portfolio.findFirst({
       where: {
         customUsername: username,
         isPublished: true
@@ -154,6 +154,7 @@ export async function getPublicPortfolio(username: string) {
           backgroundColor: true,
           backgroundPattern: true,
           cvUrl: true,
+          productHuntUsername: true,
           user: {
             select: {
               id: true,
